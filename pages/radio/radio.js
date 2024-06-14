dbf34f2bc6dd50d5babd63ce0d3164e550a96a67
MIN-GOL <imingol@qq.com>
2024-06-14 23:46:15 +0800

    feat - 歌词and播放音频

--- pages/radio/radio.js
+++ pages/radio/radio.js
@@ -1,5 +1,14 @@
 // pages/radio/radio.js
 Page({
+  playAudio() {
+    this.innerAudioContext.play();
+  },
+  pauseAudio() {
+    this.innerAudioContext.pause();
+  },
+  stopAudio() {
+    this.innerAudioContext.stop();
+  },
   // 获取歌词
   getLyrics: function () {
     var that = this;
@@ -20,12 +29,30 @@ Page({
       }
     })
   },
+  getMusicinfo: function () {
+    var that = this;
+    wx.request({
+      url: 'http://music.163.com/api/song/detail/',
+      data:({
+        id: that.data.id,
+        ids: [that.data.id]
+      }),
+      success(res){
+        var data = res.data
+        that.setData({
+          music: data.songs[0]
+        })
+      }
+    })
+  },
   /**
    * 页面的初始数据
    */
   data: {
+    url: '',
     id: '',
-    lyric: {}
+    lyric: {},
+    music: {}
   },
 
   /**
@@ -34,11 +61,36 @@ Page({
   onLoad(options) {
     var id = options.id
     this.setData({
-      id: id
+      id: id,
+      url: "http://music.163.com/song/media/outer/url?id=" + id + ".mp3"
     })
-
     this.getLyrics();
-  },
+    this.getMusicinfo();
+    // 创建 InnerAudioContext 实例
+    this.innerAudioContext = wx.createInnerAudioContext();
+    // 设置音频源
+    this.innerAudioContext.src = this.data.url;
+    // 监听音频播放事件
+    this.innerAudioContext.onPlay(() => {
+      console.log('音频开始播放');
+    });
+    // 监听音频暂停事件
+    this.innerAudioContext.onPause(() => {
+      console.log('音频暂停播放');
+    });
+    // 监听音频停止事件
+    this.innerAudioContext.onStop(() => {
+      console.log('音频停止播放');
+    });
+    // 监听音频播放结束事件
+    this.innerAudioContext.onEnded(() => {
+      console.log('音频播放结束');
+    });
+    // 监听音频播放错误事件
+    this.innerAudioContext.onError((res) => {
+      console.error('音频播放错误', res);
+    });
+  },    
 
   /**
    * 生命周期函数--监听页面初次渲染完成
