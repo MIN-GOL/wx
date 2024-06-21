9c57ab1cf05dcfe03239e83dbdc8776961d2758a
MIN-GOL <imingol@qq.com>
2024-06-21 23:51:13 +0800

    feat - 搜索歌词

--- pages/selectMusic/selectMusic.js
+++ pages/selectMusic/selectMusic.js
@@ -1,11 +1,51 @@
 // pages/selectMusic/selectMusic.js
+import Toast from '@vant/weapp/toast/toast';
 Page({
-
+  // 加载数据
+  onChange(e) {
+    this.setData({
+      value: e.detail,
+    });
+  },
+  // 搜索歌曲
+  onSearch() {
+    var that = this;
+    wx.request({
+      url: 'http://music.163.com/api/search/get/web',
+      data: {
+        csrf_token: 'hlpretag=',
+        s: this.data.value,
+        type: 1,
+        offset: 0,
+        total: true,
+        limit: 10
+      },
+      success(res){
+        const resp = res.data
+        if (resp.code === 200) {
+          Toast('搜索成功')
+          that.setData({
+            resp: resp.result
+          })
+        }else{
+          Toast('搜索失败')
+        }
+      }
+    })
+  },
+  // 转到详细页面播放器
+  toRadio: function (e) {
+    const id = e.currentTarget.id
+    wx.navigateTo({
+      url: "/pages/radio/radio?id="+id
+    })
+  },
   /**
    * 页面的初始数据
    */
   data: {
-
+    value: '',
+    resp: {}
   },
 
   /**
